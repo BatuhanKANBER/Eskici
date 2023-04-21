35794d94aa7c92985e5114278d1f2ae83aca6188
BatuhanKanber <104573473+BatuhanKanber@users.noreply.github.com>
2023-04-21 22:54:46 +0300

    feat : invoice, invoice details, order, order details,card and card details module added.

---
+++ laravel-proje/database/migrations/2023_04_21_193709_create_order_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id("order_detail_id");
+            $table->foreignIdFor(Order::class, "order_id");
+            $table->foreignIdFor(Product::class, "product_id");
+            $table->integer("quantity");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
